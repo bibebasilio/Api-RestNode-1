75c5df767e8c3400f95cad98e0ce0bfc22a7894f
bibebasilio <basiliobibe@gmail.com>
2025-10-31 15:53:38 -0300

    add notFound Middleware

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ import express from "express";
 const app = express();
 
 app.use((req, res, next) => {
-   res.json({message: "Hola Middleware"});
+    //res.json({message: "Hola API en Mantenimiento"});
+    console.log(req.method)
+        next();
 });
 
 app.get("/", (req, res) => {
-    res.send("bienvenidos a nuestra Api Rest!");
+    res.json({ message: "bienvenidos a nuestra Api Rest!" });
 });
+
+
+import notFound from "./src/middlewares/not found.js";
+
+
+app.use(notFound);
 
 
 const PORT = 3000;
