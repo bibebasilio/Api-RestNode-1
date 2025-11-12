3a91506093d0b98f68e80d1876c7b3c5d5baf34e
bibebasilio <basiliobibe@gmail.com>
2025-11-12 16:44:55 -0300

    refactor: clean up Firebase configuration formatting

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -6,13 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,  //"AIzaSyC8rtZXN4OfUQBPVOH6pmoiB6pN-gHlh7w",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,    //     "api-rest-node-7cd3f.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROYECT_ID,  //"api-rest-node-7cd3f",
-  storageBucket: process.env.FIREBASE_STRORAGE_BUCKET,   //"api-rest-node-7cd3f.firebasestorage.app",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,  // "457504115785",
-  appId:process.env.FIREBASE_APP_ID,  // "1:457504115785:web:76ec685b195aa220228398"
-};
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,   
+  projectId: process.env.FIREBASE_PROYECT_ID,
+  storageBucket: process.env.FIREBASE_STRORAGE_BUCKET,  
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
